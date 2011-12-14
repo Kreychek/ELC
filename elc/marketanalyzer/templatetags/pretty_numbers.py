058ba9f17e1e831fc2ef1563c7b20d33b8edd505
Edward Chen <kreychek@gmail.com>
2011-12-14 18:30:44 -0500

    Began styling pages w/CSS. See base.css

--- elc/marketanalyzer/templatetags/pretty_numbers.py
+++ elc/marketanalyzer/templatetags/pretty_numbers.py
@@ -3,19 +3,24 @@ import locale
 
 register = template.Library()
 
-@register.filter(name='addcommas')
-def addcommas(value, default):
+@register.filter(name='comma_trunc')
+def comma_trunc(value, default):
     """
-    A filter to add commas to numbers and display 2 decimal places. Also
+    A filter to add commas to numbers and truncate right of radix. Also
     implements the built-in ``default_if_none`` template filter's behavior.    
     That is, if (and only if) ``value`` is ``None``, use given ``default``,
     otherwise format and display ``value``.
-    
-    For example, if ``var`` contains ``15023145.3200``, then
-    ``{{ var|addcommas:"N/A" }}`` would return ``15,023,145.32``.
-    If ``var`` were equal to ``None``, then ``N/A`` would be the result.
     """    
     if value == None:
         return default
     else:
-        return locale.format('%.2f', value, grouping=True)
+        return locale.format('%.0f', value, grouping=True)
+        
+@register.filter(name='isk')
+def isk(value):
+    """
+    A filter to add the currency symbol "ISK" to the end of strings.
+    """
+    
+    if value and (value != 'N/A'):
+        return value + ' ISK'
